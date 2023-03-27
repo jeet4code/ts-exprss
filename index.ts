e78698b46508c610f89666316065e3d11d9a241b
Jeetendra chauhan <jeet4code@gmail.com>
2023-03-27 20:24:11 +0530

    added handlebars

--- index.ts
+++ index.ts
@@ -1,9 +1,26 @@
 import express, { Request, Response } from "express";
+import { engine } from 'express-handlebars';
 
 const app = express();
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+app.enable('view cache');
+
 app.get("/", (req:Request, res: Response) => {
-    res.send("Hello Friends");
+    //res.send("Hello Friends");
+    const data = {
+        title: 'My Page',
+        heading: 'Welcome to my page!',
+        showContent: true,
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+      };
+      // Update Layout
+      // app.locals.layout = "t2"
+      // res.render('home', data);
+      res.render('home', {...data, layout: "t2"});
+    
 })
 
 app.listen("3000", () => {
